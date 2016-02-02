9ff7f88ab81b92dd6fc11fa26feccc4e2a48619c
JAvy <JAvy@JAvy-PC>
2016-02-02 14:13:30 -0500

    cambioIP

--- basePresentar_AAA/src/basepresentar_aaa/Conexion.java
+++ basePresentar_AAA/src/basepresentar_aaa/Conexion.java
@@ -73,17 +73,17 @@ public class Conexion {
         String instancia="";
         if(server.equals("ADRIAN")){
             //instancia="192.168.1.6";
-              instancia="172.21.103.49";
+              instancia="172.21.105.124";
         }else{
             if(server.equals("JAVY-PC")){
-                //instancia="192.168.1.3";
-                instancia="172.21.103.117";
+               // instancia="192.168.1.3";
+               instancia="172.21.105.123";
             }else{
-                if(server.equals("TOSHIBA")){
-                  //  instancia="192.168.1.4";
-                    instancia="172.21.103.215";
-                }
-        }
+                
+                //   instancia="192.168.1.4";
+                   instancia="172.21.105.248";
+                
+            }
             }
             JOptionPane.showMessageDialog(null, instancia);
         

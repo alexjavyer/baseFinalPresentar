43c4555ad96a2ef085d86117e1a11749b927ee5f
JAvy <JAvy@JAvy-PC>
2016-02-02 10:27:38 -0500

    cambios IP

--- basePresentar_AAA/src/basepresentar_aaa/Conexion.java
+++ basePresentar_AAA/src/basepresentar_aaa/Conexion.java
@@ -30,10 +30,12 @@ public class Conexion {
                 String instancia="";
                 
         if(server.equals("ADRIAN")){
-            instancia="192.168.1.6";
+            //instancia="192.168.1.6";
+              instancia="172.21.103.49";
         }else{
             if(server.equals("JAVY-PC")){
-                instancia="192.168.1.3";
+                //instancia="192.168.1.3";
+                instancia="172.21.103.117";
             }else{
                 if(server.equals("TOSHIBA")){
                     //instancia="192.168.1.4";

825bbde8b90ac4ad642901062e46f48596d7f3c2
JAvy <JAvy@JAvy-PC>
2016-02-13 22:27:19 -0500

    snap

--- basePresentar_AAA/src/basepresentar_aaa/Instantanea1.java
+++ basePresentar_AAA/src/basepresentar_aaa/Instantanea1.java
@@ -430,6 +430,7 @@ public class Instantanea1 extends javax.swing.JInternalFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+
         // TODO add your handling code here:
 //        filtrosTabla();
         //publicacionFiltros(txtNombrePublicacion.getText());
